bb578eb493fc047c0eeec17d570c1ff0f6c4f7f7
oranheim <oranheim@gmail.com>
2024-10-08 10:52:36 +0200

    Add module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module messi.provider.filesystem {
+    requires messi.sdk;
+    requires property.config;
+
+    requires org.slf4j;
+    requires org.apache.avro;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
+
+    provides no.cantara.messi.api.MessiClientFactory with no.cantara.messi.filesystem.FilesystemMessiClientFactory;
+
+    exports no.cantara.messi.avro;
+    exports no.cantara.messi.filesystem;
+}
